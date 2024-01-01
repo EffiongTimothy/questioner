839e5199b96b9a86ac412812452c21be3b4345eb
timothy <effiongtimothy1@gmail.com>
2024-01-01 08:43:02 +0100

    created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Questioner from './component/question';
+
+function App() {
+  return (
+    <div className="App">
+  <Questioner/>
+    </div>
+  );
+}
+
+export default App;
